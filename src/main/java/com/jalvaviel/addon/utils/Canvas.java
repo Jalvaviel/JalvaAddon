d89ea4ae10c5e9101493559b807ba500dd6d91ae
Jalvaviel <jalvaviel@gmail.com>
2024-02-14 13:26:56 +0100

    Canvas basic generation

--- src/main/java/com/jalvaviel/addon/utils/Canvas.java
+++ src/main/java/com/jalvaviel/addon/utils/Canvas.java
@@ -1,5 +1,6 @@
 package com.jalvaviel.addon.utils;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.UUID;
@@ -7,12 +8,65 @@ import java.util.UUID;
 public class Canvas {
     int width = 0;
     int height = 0;
-    boolean mapBg = false;
-    ArrayList<ArrayList<Map>> mapMatrix = new ArrayList<>();
+    ArrayList<Map> mapMatrix = new ArrayList<>();
     UUID canvasID = null;
     BufferedImage bufferedCanvas = null;
 
-    public enum canvasType{
+    public Canvas(ArrayList<Map> mapMatrix, CanvasType canvasType){
+        this.mapMatrix = mapMatrix;
+        generateCanvas(canvasType);
+        drawCanvas();
+    }
+
+    private void generateCanvas(CanvasType canvasType){
+        switch(canvasType){
+            case PLAYER_INVENTORY:
+                this.width = 128 * 9;
+                this.height = 128 * 4;
+                break;
+            case CHEST:
+                this.width = 128 * 9;
+                this.height = 128 * 3;
+                break;
+            case DOUBLE_CHEST:
+                this.width = 128 * 9;
+                this.height = 128 * 6;
+                break;
+            case DISPENSER:
+                this.width = 128 * 3;
+                this.height = 128 * 3;
+                break;
+            case SINGLE:
+                this.width = 128;
+                this.height = 128;
+                break;
+            case CUSTOM:
+                // TODO
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void drawCanvas() {
+        BufferedImage resultImage = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = resultImage.createGraphics();
+        int mapIndex = 0;
+        for (int y = 0; y < this.height; y += 128) {
+            for (int x = 0; x < this.width; x += 128) {
+                if (mapIndex < mapMatrix.size()) {
+                    Map map = mapMatrix.get(mapIndex++);
+                    graphics.drawImage(map.bufferedMap, x, y, null);
+                } else {
+                    break;
+                }
+            }
+        }
+        graphics.dispose();
+        this.bufferedCanvas = resultImage;
+    }
+
+    public enum CanvasType{
         CUSTOM,
         PLAYER_INVENTORY,
         CHEST,
