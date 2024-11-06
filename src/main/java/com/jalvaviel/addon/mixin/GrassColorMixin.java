a7990e736404ec35505cac653082939eedcbe394
Jalvaviel <jalvaviel@gmail.com>
2024-11-06 20:11:22 +0100

    Changed the code for BiomeColorChanger to accept RegKeys of Biomes.

---
+++ src/main/java/com/jalvaviel/addon/mixin/GrassColorMixin.java
@@ -0,0 +1,35 @@
+package com.jalvaviel.addon.mixin;
+
+import com.jalvaviel.addon.modules.BiomeColorChanger;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
+
+@Mixin(BiomeColors.class)
+public class GrassColorMixin {
+    @Inject(method = "getGrassColor", at = @At("HEAD"), cancellable = true)
+    private static void onGetGrassColor(BlockRenderView world, BlockPos pos, CallbackInfoReturnable<Integer> info) {
+        BiomeColorChanger biomeColorChanger = Modules.get().get(BiomeColorChanger.class);
+        if (biomeColorChanger.isActive()) {
+            assert mc.world != null;
+            assert mc.player != null;
+            try {
+                RegistryEntry<Biome> currentBiome = mc.world.getBiome(pos);
+                if (biomeColorChanger.biomes.get().contains(currentBiome.getIdAsString())) {
+                    info.setReturnValue(biomeColorChanger.biomeConfigs.get().get(currentBiome.getIdAsString()).grassColor.getPacked());
+                }
+            } catch (Exception ignored) {}
+        }
+    }
+}
+
